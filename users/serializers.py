9f5c68bf7c4ad9322aca85442f93e0d82cedd1a2
paul <paul.th.cartier@gmail.com>
2024-03-19 14:21:52 +0100

    add UserDetailSerializer, fix UserSerializer for create & update

--- users/serializers.py
+++ users/serializers.py
@@ -15,3 +15,31 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username', 'email', 'profile']
+
+    def create(self, validated_data):
+        profile_data = validated_data.pop('profile')
+        user = User.objects.create(**validated_data)
+        Profile.objects.create(user=user, **profile_data)
+        return user
+    
+    def update(self, instance, validated_data):
+        instance.username = validated_data.get('username', instance.username)
+        instance.email = validated_data.get('email', instance.email)
+        instance.save()
+
+        profile_data = validated_data.pop('profile', {})
+        profile = instance.profile
+        profile.hometown = profile_data.get('hometown', profile.hometown)
+        profile.age = profile_data.get('age', profile.age)
+        profile.gender = profile_data.get('gender', profile.gender)
+        profile.save()
+
+        return instance
+    
+
+class UserDetailSerializer(serializers.ModelSerializer):
+    profile = ProfileSerializer()
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'date_joined', 'profile']
--- users/views.py
+++ users/views.py
@@ -1,7 +1,12 @@
 from rest_framework import viewsets
 from django.contrib.auth.models import User
-from .serializers import UserSerializer
+from .serializers import UserSerializer, UserDetailSerializer
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+    def get_serializer_class(self):
+        if self.action == 'retrieve':
+            return UserDetailSerializer
+        return UserSerializer
